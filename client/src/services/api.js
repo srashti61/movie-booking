8463997f08639cd9708af610ae934a6c0e632958
Srashti Gupta <srashty63@gmail.com>
2026-03-31 16:06:46 +0530

    set frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' }
 });
 
