a9f73e1877c30526b82ae3c93fe8e9e6d437d19b
Srashti Gupta <srashty63@gmail.com>
2026-04-29 20:26:29 +0530

    done

--- server/server.js
+++ server/server.js
@@ -13,13 +13,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || origin.includes("vercel.app")) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "https://movie-booking-vn6i.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json());
